07a1e7ade3f1d908dfe32bd8c9ef87ffbd7b6956
vlapin <Vyacheslav.Lapin@gmail.com>
2020-08-05 15:41:19 +0300

    Initial commit

---
+++ src/test/java/ru/vlapin/experiments/rabbitmqluxoftcourse/RabbitmqLuxoftCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vlapin.experiments.rabbitmqluxoftcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqLuxoftCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
